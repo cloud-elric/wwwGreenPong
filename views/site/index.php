e79ad4e992d1f0f99f9e829b9d5ab91181e65e36
damsSanch <damian2gom@gmail.com>
2016-10-03 10:56:43 -0500

    Aviso de Privacidad y Terminos y Condiciones

--- views/site/index.php
+++ views/site/index.php
@@ -69,16 +69,16 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 			<div class="aceptar-mask"></div>
 
 			<!-- .modal -->
-			<div id="modal-terminos-condiciones" class="modal modal-aviso-privacidad">
+			<div id="modal-terminos-condiciones" class="modal modal-terminos-condiciones">
 
 				<!-- Modal content -->
 				<div class="modal-content">
 					<div class="modal-header">
 						<span class="close modal-terminos-condiciones-close">×</span>
-						<h2>Términos-condiciones</h2>
+						<h2>Términos y Condiciones</h2>
 					</div>
 					<div class="modal-body">
-						<div class="aviso-privacidad-colum">
+						<div class="terminos-condiciones-colum">
 							<p>
 								<strong>IMPULSORA DE VACACIONES FIESTA SA DE CV</strong>  GOP0112128B4 (que en lo sucesivo será denominado <strong>FIESTA AMERICANA)</strong> con domicilio en Prolongación Paseo de Reforma #1015 Piso 9, Col. Santa Fe, Del. Álvaro Obregón, C.P. 01210, Ciudad de México y <strong>PUBLICIDAD Y SOLUCIONES GREEN SA DE CV</strong> (que en los sucesivo de será denominada P.GREEN) con domicilio en Santa Brígida 19, Jardines de Santa Mónica, C.P. 54055, Tlalnepantla de Baz Estado de México. Ponen a disposición del público en general los siguientes términos y condiciones de la promoción <strong>“MASTER EXPERIENCE”</strong> (que en lo sucesivo será denominada LA PROMOCIÓN). 
 							</p>
@@ -124,7 +124,7 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							<h4>VI. ESTABLECIMIENTOS PARTICIPANTES.</h4>
 
 							<p>
-								La mecánica se realizara por medio del sitio web <a href="www.participagana.com.mx/abiertomonterrey" target="_blank">www.participagana.com.mx/abiertomonterrey</a>
+								La mecánica se realizara por medio del sitio web <a class="link" href="www.participagana.com.mx/abiertomonterrey" target="_blank">www.participagana.com.mx/abiertomonterrey</a>
 							</p>
 
 							<h4>
@@ -132,7 +132,7 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							</h4>
 
 							<p>
-								Participan personas mayores de 18 años, que ingresen al sitio web <a href="www.participagana.com.mx/abiertomonterrey" target="_blank">www.participagana.com.mx/abiertomonterrey</a>  cada registro obtendrá una oportunidad de participar en el juego de “ping pong”. Participan para ganar un pase doble para las finales del ATP 2016. 
+								Participan personas mayores de 18 años, que ingresen al sitio web <a class="link" href="www.participagana.com.mx/abiertomonterrey" target="_blank">www.participagana.com.mx/abiertomonterrey</a>  cada registro obtendrá una oportunidad de participar en el juego de “ping pong”. Participan para ganar un pase doble para las finales del ATP 2016. 
 							</p>
 
 							<ol type="1">
@@ -145,10 +145,10 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 								<li>
 									<h4>Mecánica</h4>
 									<p>
-										Los participantes, podrán jugar en el sitio web www.participagana.com.mx/abiertomonterrey “ping pong” para acumular puntos y participar por un pase doble para la final del ATP 2016, el juego en automático guardara el puntaje así como el registro tomando en cuenta fecha y hora, al terminar le indicarán la puntuación acumulada y horario de participación. 
+										Los participantes, podrán jugar en el sitio web <a href="www.participagana.com.mx/abiertomonterrey" class="link">www.participagana.com.mx/abiertomonterrey</a> “ping pong” para acumular puntos y participar por un pase doble para la final del ATP 2016, el juego en automático guardara el puntaje así como el registro tomando en cuenta fecha y hora, al terminar le indicarán la puntuación acumulada y horario de participación. 
 									</p>
 									<p>
-										La vigencia para participar será del 3 de octubre al 13 de octubre. A las 15:00 hrs. Del 13 de octubre de 2016, se publicara el nombre y puntaje ganador en el sitio web www.participagana.com.mx/abiertomonterrey así como en la página www.facebook.com/ParticipaGanaOficial  
+										La vigencia para participar será del 3 de octubre al 13 de octubre. A las 15:00 hrs. Del 13 de octubre de 2016, se publicara el nombre y puntaje ganador en el sitio web <a href="" class="link">www.participagana.com.mx/abiertomonterrey</a> así como en la página <a href="" class="link">www.facebook.com/ParticipaGanaOficial</a>
 									</p>
 									<p>
 										En caso de contar con dos o más participantes con la misma puntuación se tomará en cuenta al participante que  haya registrado primero en fecha y horario su participación.
@@ -160,18 +160,29 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 								<li>
 									<h4>Premios</h4>
 									<p>
-										FIESTA AMERICANA ha dispuesto para esta PROMOCIÓN certificados de regalo sin valor nominal distribuidos de la siguiente manera: 
+										FIESTA AMERICANA ha dispuesto para esta PROMOCIÓN certificados de regalo <strong>sin valor nominal</strong> distribuidos de la siguiente manera: 
 									</p>
+
+									<table>
+										<tr>
+											<th class="table-score">SCORE + ALTO</th>
+											<th>1 pase doble para las finales del ATP 2016</th>
+										</tr>
+										<tr>
+											<td>Todos ganan certificado 2x1 Fiesta Americana</td>
+											<td>Una noche gratis en la compra mínima de una en los Hoteles Fiesta Americana participantes, (para consultar destinos y hoteles participantes marcar a los números 01 800 504 5000 al 5326 6900).</td> 
+										</tr>
+									</table>
 								</li>
 								<li>
 									<h4>
 										Restricciones para la entrega del pase doble  a las finales de ATP 2016
 									</h4>
 									<p>
-										El 13 de octubre de 2016 se dará a conocer al ganador de este incentivo, a través del sitio web, así como en la página   www.facebook.com/ParticipaGanaOficial 
+										El 13 de octubre de 2016 se dará a conocer al ganador de este incentivo, a través del sitio web, así como en la página <a href="www.facebook.com/ParticipaGanaOficial" class="link">www.facebook.com/ParticipaGanaOficial</a>
 									</p>
 									<p>
-										Al ganador se  le contactará a los números que proporciono en su registro. Contará con 24 horas para enviar al correo electrónico promociones@participagana.com.mx  la siguiente documentación:
+										Al ganador se  le contactará a los números que proporciono en su registro. Contará con 24 horas para enviar al correo electrónico <a href="mailto:promociones@participagana.com.mx" class="link">promociones@participagana.com.mx</a>  la siguiente documentación:
 									</p>
 									<ul>
 										<li>
@@ -217,6 +228,17 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							<p>
 								FIESTA AMERICANA ha dispuesto para fines de LA PROMOCIÓN los siguientes incentivos:
 							</p>
+									
+							<table>
+								<tr>
+									<th class="table-score">SCORE + ALTO</th>
+									<th>1 pase doble para las finales del ATP 2016</th>
+								</tr>
+								<tr>
+									<td>Todos ganan certificado 2x1 Fiesta Americana</td>
+									<td>Una noche gratis en la compra mínima de una en los Hoteles Fiesta Americana participantes, (para consultar destinos y hoteles participantes marcar a los números 01 800 504 5000 al 5326 6900).</td> 
+								</tr>
+							</table>
 
 							<h4>IX. AUTORIZACIÓN.</h4>
 
@@ -299,11 +321,11 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							</p>
 
 							<h4>
-								PRIVACIDAD Y PROTECCIÓN DE DATOS PERSONALES.
+								XI. PRIVACIDAD Y PROTECCIÓN DE DATOS PERSONALES.
 							</h4>
 
 							<p>
-								IMPULSORA DE VACACIONES FIESTA SA DE CV, pone a disposición de los consumidores y participantes de LA PROMOCIÓN su aviso de privacidad, que puede ser consultado íntegramente en la página web: www.posadas.com/es/privacidad 
+								IMPULSORA DE VACACIONES FIESTA SA DE CV, pone a disposición de los consumidores y participantes de LA PROMOCIÓN su aviso de privacidad, que puede ser consultado íntegramente en la página web: <a href="www.posadas.com/es/privacidad" class="link">www.posadas.com/es/privacidad</a>
 							</p>
 
 							<p>
@@ -315,7 +337,7 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							</p>
 
 							<p>
-								Usted podrá consultar el Aviso de Privacidad completo publicado en la página de internet, www.participagana.com.mx 
+								Usted podrá consultar el Aviso de Privacidad completo publicado en la página de internet, <a href="" class="link">www.participagana.com.mx</a>
 							</p>
 
 							<h4>XII. MODIFICACIONES A LOS TÉRMINOS Y CONDICIONES.</h4>
@@ -333,7 +355,7 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							<h4>XIV. PUBLICACIÓN DE LOS GANADORES.</h4>
 
 							<p>
-								La publicación del ganador se realizará el día 13 de octubre en el sitio web www.participagana.com.mx/abiertomonterrey y  en la Página de Internet www.facebook.com/ParticipaGanaOficial
+								La publicación del ganador se realizará el día 13 de octubre en el sitio web <a href="www.participagana.com.mx/abiertomonterrey" class="link">www.participagana.com.mx/abiertomonterrey</a> y  en la Página de Internet <a href="www.facebook.com/ParticipaGanaOficial" class="link">www.facebook.com/ParticipaGanaOficial</a>
 							</p>
 
 							<p>
@@ -343,7 +365,7 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 							<h4>XV. CONTACTO.</h4>
 
 							<p>
-								Para cualquier aclaración o información relacionada con LA PROMOCIÓN, los únicos medios autorizados por FIESTA AMERICANA son promociones@participagana.com.mx y el sistema telefónico de información 01-800-467187 de cobertura nacional.
+								Para cualquier aclaración o información relacionada con LA PROMOCIÓN, los únicos medios autorizados por FIESTA AMERICANA son <a href="mailto:promociones@participagana.com.mx" class="link">promociones@participagana.com.mx</a> y el sistema telefónico de información 01-800-467187 de cobertura nacional.
 							</p>
 
 							<h4>XVI.QUEJAS.</h4>
@@ -381,22 +403,102 @@ $this->registerJsFile('@web/js/login.js',['depends' => [\app\assets\AppAsset::cl
 	<!-- end - .aviso-privacidad -->
 
 	<!-- .modal (Aviso de privacidad) -->
-	<div id="modal-aviso-privacidad" class="modal">
+	<div id="modal-aviso-privacidad" class="modal modal-aviso-privacidad">
 
 		<!-- .modal-content -->
 		<div class="modal-content">
 			<!-- .modal-header -->
 			<div class="modal-header">
 				<span class="close modal-aviso-privacidad-close">×</span>
-				<h2>Aviso de privacidad</h2>
+				<h2>Aviso de privacidad de PUBLICIDAD GREEN</h2>
 			</div>
 			<!-- end - .modal-header -->
 			<!-- .modal-body -->
 			<div class="modal-body">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
-					aliquam, sunt nesciunt aperiam quis, tenetur libero vero fugit
-					cupiditate nisi! Delectus quis vel cum explicabo incidunt eius
-					rerum enim laborum.</p>
+				<div class="aviso-privacidad-colum">
+					
+					<p>
+						Con fundamento en los artículos 15 y 16 de la <strong>LEY FEDERAL DE PROTECCIÓN DE DATOS PERSONALES EN POSESIÓN DE PARTICULARES,</strong> hacemos de su conocimiento que, PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., con domicilio en calle Convento de Santa Brígida #19, Colonia Jardines de Santa Mónica, Tlalnepantla, Estado de México, C.P. 54050, es responsable de recabar sus datos personales, del uso que se les dé a los mismos y de su protección.
+					</p>
+
+					<p>
+						Su información personal será utilizada para las siguientes finalidades: proveer los servicios y productos que ha solicitado; notificarle sobre nuevos productos que tengan relación con los ya contratados o adquiridos; comunicarle en los cambios de los mismos; elaborar estudios y programas que son necesarios para determinar hábitos de consumo; realizar evaluaciones periódicas de nuestros productos y servicios para efecto de mejorar la calidad de los mismos; evaluar la calidad del servicio que brindamos, y en general  para dar cumplimiento a las obligaciones que hemos contraído para con Usted.
+					</p>
+
+					<p>
+						Para las finalidades antes mencionadas, en forma enunciativa más no limitativa podríamos obtener alguno de los siguientes datos personales: nombre completo, teléfono fijo y/o teléfono celular, correo electrónico, dirección de facebook, twiter, y/o cualquier otra red social, así como domicilio particular y de trabajo.
+					</p>
+
+					<h4>LIMITACIÓN DE USO Y DIVULGACIÓN</h4>
+
+					<p>
+						El tratamiento de sus datos personales será el que resulte necesario, adecuado y relevante en relación con las finalidades previstas en esta Política de Privacidad.
+					</p>
+
+					<p>
+						PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., cumple los principios de protección de datos personales establecidos en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares y adopta las medidas necesarias para su aplicación. Lo anterior aplica aún y cuando estos datos fueren tratados por un tercero, a solicitud de PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., y con el fin de cubrir los servicios necesarios, manteniendo la confidencialidad en todo momento. PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., toma las medidas necesarias y suficientes para procurar que esta Política de Privacidad sea respetada, por él o por terceros con los que guarde alguna relación, para otorgar los servicios o productos establecidos con el titular.
+					</p>
+
+					<p>
+						La obligación de acceso a la información se dará por cumplida cuando se ponga a disposición del titular los datos personales, o bien, mediante la expedición de copias simples, documentos electrónicos o cualquier otro medio que PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., provea al titular.
+					</p>
+
+					<p>
+						En el caso de que el titular solicite acceso a los datos a una persona que presume es el responsable, y ésta resulta no serlo, bastará con que así se le indique al titular por cualquiera de los medios impresos (carta de procedencia) o electrónicos (correo electrónico, medios audiovisuales, etc.), para tener por cumplida la solicitud. 
+					</p>
+
+					<p>
+						PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., limitará el uso de los datos personales sensibles a petición expresa del titular, y no estará obligada a cancelar los datos personales cuando:
+					</p>
+
+					<ol type="1">
+						<li>Se refiera a las partes de un contrato privado, social, administrativo, y sean necesarios para su desarrollo y cumplimiento.</li>
+						<li>Deban ser tratados por disposición legal.</li>
+						<li>Obstaculice actuaciones judiciales o administrativas vinculadas a obligaciones fiscales, la investigación y persecución de delitos, o la actualización de sanciones administrativas.</li>
+						<li>Sean necesarios para proteger los intereses jurídicamente tutelados del titular.</li>
+						<li>Sean necesarios para realizar una acción en función del interés público.</li>
+						<li>Sean necesarios para cumplir con una obligación legalmente adquirida por el titular, o…</li>
+						<li>Sean objeto de tratamiento para la prevención o el diagnóstico médico o la gestión de servicios de salud, siempre que dicho tratamiento se realice por un profesional de la salud sujeto a un deber secreto.</li>
+					</ol>
+
+					<p>
+						<strong>
+							Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar su consentimiento, para lo cual se puede poner en contacto directamente a Publicidad y Soluciones Green S.A. de C.V., la cual puede localizar en la dirección ya señalada en el primer párrafo de este aviso, o al teléfono 24-87-01-01, y en el correo electrónico <a href="mailto:info@publicidadgreen.com.mx" class="link">info@publicidadgreen.com.mx</a>, o bien a través de un escrito libre acompañado de una copia de su identificación oficial, y especificando los datos personales que quiera proteger, entregándolo en la misma dirección ya citada,
+						</strong>
+						PUBLICIDAD Y SOLUCIONES GREEN S.A. DE C.V., contará con 20 días hábiles para atender su solicitud.
+					</p>
+
+					<p>
+						Si usted no manifiesta su oposición para que sus datos personales sean transferidos, se entenderá que ha otorgado su consentimiento para ello.
+					</p>
+
+					<p>
+						<strong>
+							Es importante informarle que Usted tiene derecho al acceso, ratificación, y cancelación de sus datos personales, a oponerse al tratamiento de los mismos o a revocar el consentimiento que para dicho fin nos haya otorgado.
+						</strong>
+					</p>
+
+					<p>
+						Al aceptar las condiciones y términos establecidos por medio del presente aviso de privacidad se considera otorgado el consentimiento expreso para que PUBLICIDAD GREEN, haga uso de manejo tratamiento y transferencia de sus datos personales para los fines descritos.
+					</p>
+
+					<h4>
+						CAMBIOS A ESTA DECLARACIÓN DE PREIVACIDAD
+					</h4>
+
+					<p>
+						<strong>SOLUCIONES GREN S.A. DE C.V.</strong> podrá en cualquier momento actualizar esta declaración de privacidad. En el caso de que se produzcan cambios sustanciales a esta declaración, lo comunicaremos a través de nuestras páginas de internet señaladas en el párrafo tres de este Aviso de Privacidad.
+					</p>
+
+					<p>
+						Por lo anterior, les sugerimos visitar periódicamente esta declaración de privacidad
+					</p>
+
+					<p>
+						Contacto PUBLICIDAD GREEN: <a href="mailto:info@publicidadgreen.com.mx" class="link">info@publicidadgreen.com.mx</a>
+					</p>
+
+				</div>
 			</div>
 			<!-- end - .modal-body -->
 		</div>
