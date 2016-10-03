417cec13f2e92efaae4270079edb7e58bdb0d566
damsSanch <damian2gom@gmail.com>
2016-10-03 14:28:19 -0500

    Código no válido

--- views/site/codigoNoValido.php
+++ views/site/codigoNoValido.php
@@ -1,1 +1,21 @@
-<h1>Codigo no valido o vencido</h1>
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+use yii\helpers\Url;
+
+?>
+
+<!-- .codigo-no-valido -->
+<div class="codigo-no-valido">
+	<!-- .codigo-no-valido-cont -->
+	<div class="codigo-no-valido-cont">
+
+		<h3>Código no válido o vencido</h3>
+
+		<a href="<?=Url::base()?>/site/ingresar-codigo">Vuelve a intentarlo</a>
+
+	</div>
+	<!-- end - .codigo-no-valido-cont -->
+</div>
+<!-- end- .codigo-no-valido -->
