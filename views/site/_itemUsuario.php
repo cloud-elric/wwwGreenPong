8ddffaa33905a1a0df61595b6e483a34d67aa0d5
damsSanch <damian2gom@gmail.com>
2016-10-05 18:37:37 -0500

    Usuarios registrados (css)

--- views/site/_itemUsuario.php
+++ views/site/_itemUsuario.php
@@ -1,5 +1,5 @@
-<div class="puntuacion-cont-tabla-body-tr">
-	<div class="puntuacion-cont-tabla-body-td"><?=$model->nombreCompleto?></div>
-	<div class="puntuacion-cont-tabla-body-td"><?=$model->tel_numero_celular?></div>
+<div class="user-register-cont-tabla-body-tr">
+	<div class="user-register-cont-tabla-body-td"><?=$model->nombreCompleto?></div>
+	<div class="user-register-cont-tabla-body-td"><?=$model->tel_numero_celular?></div>
 	
 </div>
